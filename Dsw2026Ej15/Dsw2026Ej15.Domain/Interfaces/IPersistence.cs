7a04e168d4c167017cff150f5a4b70496385ce69
Nahiara Schiaffino <nahiara.schiaffino@alu.frt.utn.edu.ar>
2026-06-19 17:48:23 -0300

    Data y Persistencia

---
+++ Dsw2026Ej15/Dsw2026Ej15.Domain/Interfaces/IPersistence.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dsw2026Ej15.Domain.Entities;
+
+namespace Dsw2026Ej15.Domain.Interfaces
+{
+    public interface IPersistence
+    {
+        Speciality? GetSpecialityById(Guid id);
+        void SaveDoctor(Doctor doctor);
+        List<Doctor> GetAllDoctors();
+        Doctor? GetDoctorById(Guid id);
+        void ToggleDoctorActive(Guid id);
+    }
+}
